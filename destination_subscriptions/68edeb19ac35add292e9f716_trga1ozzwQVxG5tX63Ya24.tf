05f3af8a208b3f8e4e3ceb696697de50d5be8832
Segment Git Sync <friends@segment.com>
2025-11-03 10:28:28 +0000

    Destination Subscription 'trga1ozzwQVxG5tX63Ya24' modified by coco.chung@jkos.com

--- destination_subscriptions/68edeb19ac35add292e9f716_trga1ozzwQVxG5tX63Ya24.tf
+++ destination_subscriptions/68edeb19ac35add292e9f716_trga1ozzwQVxG5tX63Ya24.tf
@@ -23,27 +23,10 @@ resource "segment_destination_subscription" "id-68edeb19ac35add292e9f716_trga1oz
       "@path" = "$.messageId"
     }
     properties = {
-      交易單號 = {
-        "@path" = "$.properties.purchase_no"
-      }
-      交易時間 = {
-        "@path" = "$.properties.transaction_time"
-      }
-      交易狀態 = {
-        "@path" = "$.properties.purchase_status"
-      }
-      交易金額 = {
-        "@path" = "$.properties.total_amount"
-      }
-      支付方式 = {
-        "@path" = "$.properties.pay_type"
-      }
-      支付金額 = {
-        "@path" = "$.properties.pay_amount"
-      }
+      "@path" = "$.properties"
     }
     timestamp = {
-      "@path" = "$.receivedAt"
+      "@path" = "$.properties.transaction_time"
     }
     user_id = {
       "@path" = "$.properties.account_id"
